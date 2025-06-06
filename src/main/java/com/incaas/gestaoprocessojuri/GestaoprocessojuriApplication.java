7b8bc028cb5e5ebd182fd6161c67a91d821f660f
lucas-PI <lucas22inacio@hotmail.com>
2025-06-06 13:45:33 -0300

    projeto criado

---
+++ src/main/java/com/incaas/gestaoprocessojuri/GestaoprocessojuriApplication.java
@@ -0,0 +1,13 @@
+package com.incaas.gestaoprocessojuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoprocessojuriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoprocessojuriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/incaas/gestaoprocessojuri/GestaoprocessojuriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incaas.gestaoprocessojuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoprocessojuriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
