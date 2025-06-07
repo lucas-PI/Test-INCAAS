2c6ce8506c41a7499993f3d3d238906a4a495766
lucas-PI <lucas22inacio@hotmail.com>
2025-06-07 18:35:48 -0300

    CRUD completo classe ProcessoJudicial controller

--- src/main/java/com/incaas/gestaoprocessojuri/controllers/ProcessoJudicialController.java
+++ src/main/java/com/incaas/gestaoprocessojuri/controllers/ProcessoJudicialController.java
@@ -1,23 +1,53 @@
 package com.incaas.gestaoprocessojuri.controllers;
 
+import com.incaas.gestaoprocessojuri.dtos.RequestPostProcessoJudicial;
+import com.incaas.gestaoprocessojuri.dtos.RequestPutProcessoJudicial;
 import com.incaas.gestaoprocessojuri.model.ProcessoJudicial;
 import com.incaas.gestaoprocessojuri.services.ProcessoJudicialService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/processos")
 public class ProcessoJudicialController {
 
+    @Autowired
     private ProcessoJudicialService processoJudicialService;
 
     @GetMapping
     public ResponseEntity<List<ProcessoJudicial>>findALl(){
         List<ProcessoJudicial> judicialList = processoJudicialService.findAll();
         return ResponseEntity.ok().body(judicialList);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ProcessoJudicial>findById(@PathVariable Long id){
+        ProcessoJudicial obj = processoJudicialService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        processoJudicialService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<ProcessoJudicial> insert(@RequestBody @Valid RequestPostProcessoJudicial objDto){
+        ProcessoJudicial obj = processoJudicialService.insert(objDto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProcessoJudicial> replace(@PathVariable Long id,
+                                                    @RequestBody @Valid RequestPutProcessoJudicial objDto){
+     return ResponseEntity.ok().body(processoJudicialService.replace(id, objDto));
     }
 }
--- src/main/java/com/incaas/gestaoprocessojuri/services/ProcessoJudicialService.java
+++ src/main/java/com/incaas/gestaoprocessojuri/services/ProcessoJudicialService.java
@@ -1,9 +1,13 @@
 package com.incaas.gestaoprocessojuri.services;
 
+import com.incaas.gestaoprocessojuri.dtos.RequestPostProcessoJudicial;
+import com.incaas.gestaoprocessojuri.dtos.RequestPutProcessoJudicial;
+import com.incaas.gestaoprocessojuri.mapper.ProcessoJudicialMapper;
 import com.incaas.gestaoprocessojuri.model.ProcessoJudicial;
 import com.incaas.gestaoprocessojuri.repositories.ProcessoJudicialRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,16 +17,28 @@ public class ProcessoJudicialService {
     @Autowired
     private ProcessoJudicialRepository processoJudicialRepository;
 
+    @Transactional(readOnly = true)
     public List<ProcessoJudicial> findAll(){
         return processoJudicialRepository.findAll();
     }
-
+    @Transactional(readOnly = true)
     public ProcessoJudicial findById(Long id){
         ProcessoJudicial obj = processoJudicialRepository.findById(id).get();
         return obj;
     }
-
+    @Transactional
     public void delete(Long id){
         processoJudicialRepository.deleteById(id);
+    }
+    @Transactional
+    public ProcessoJudicial insert(RequestPostProcessoJudicial objDto){
+        ProcessoJudicial processoJudicial = ProcessoJudicialMapper.INSTANCE.toProcessoJudicial(objDto);
+        return processoJudicialRepository.save(processoJudicial);
+    }
+    public ProcessoJudicial replace(Long id,RequestPutProcessoJudicial objDto){
+        ProcessoJudicial obj = ProcessoJudicialMapper.INSTANCE.toProcessoJudicial(objDto);
+        ProcessoJudicial oldObj = findById(id);
+        obj.setId(oldObj.getId());
+        return processoJudicialRepository.save(obj);
     }
 }
