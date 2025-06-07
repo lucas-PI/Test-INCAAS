38c8780f7441282ab4120497e0b8fdcd3eb71032
lucas-PI <lucas22inacio@hotmail.com>
2025-06-07 18:37:23 -0300

    classes dtos criadas para auxiliar nos metodos put e post

---
+++ src/main/java/com/incaas/gestaoprocessojuri/dtos/RequestPostProcessoJudicial.java
@@ -0,0 +1,63 @@
+package com.incaas.gestaoprocessojuri.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.incaas.gestaoprocessojuri.model.Audiencia;
+import com.incaas.gestaoprocessojuri.model.enums.ProcessoJudicialStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RequestPostProcessoJudicial {
+
+    @Pattern(regexp = "^[0-9]{7}-[0-9]{2}[.][0-9]{4}[.][0-9][.][0-9]{2}[.][0-9]{4}$")
+    private String numeroProcesso;
+    private String vara;
+    private String comarca;
+    @Enumerated(EnumType.STRING)
+    private ProcessoJudicialStatus status;
+
+    public RequestPostProcessoJudicial() {
+    }
+
+    public RequestPostProcessoJudicial(String numeroProcesso, String vara,
+                                       String comarca, ProcessoJudicialStatus status) {
+        this.numeroProcesso = numeroProcesso;
+        this.vara = vara;
+        this.comarca = comarca;
+        this.status = status;
+    }
+
+    public String getNumeroProcesso() {
+        return numeroProcesso;
+    }
+
+    public void setNumeroProcesso(String numeroProcesso) {
+        this.numeroProcesso = numeroProcesso;
+    }
+
+    public String getVara() {
+        return vara;
+    }
+
+    public void setVara(String vara) {
+        this.vara = vara;
+    }
+
+    public String getComarca() {
+        return comarca;
+    }
+
+    public void setComarca(String comarca) {
+        this.comarca = comarca;
+    }
+
+    public ProcessoJudicialStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ProcessoJudicialStatus status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/com/incaas/gestaoprocessojuri/dtos/RequestPutProcessoJudicial.java
@@ -0,0 +1,59 @@
+package com.incaas.gestaoprocessojuri.dtos;
+
+import com.incaas.gestaoprocessojuri.model.enums.ProcessoJudicialStatus;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.Pattern;
+
+public class RequestPutProcessoJudicial {
+
+    @Pattern(regexp = "^[0-9]{7}-[0-9]{2}[.][0-9]{4}[.][0-9][.][0-9]{2}[.][0-9]{4}$")
+    private String numeroProcesso;
+    private String vara;
+    private String comarca;
+    @Enumerated(EnumType.STRING)
+    private ProcessoJudicialStatus status;
+
+    public RequestPutProcessoJudicial() {
+    }
+
+    public RequestPutProcessoJudicial(String numeroProcesso, String vara,
+                                      String comarca, ProcessoJudicialStatus status) {
+        this.numeroProcesso = numeroProcesso;
+        this.vara = vara;
+        this.comarca = comarca;
+        this.status = status;
+    }
+
+    public String getNumeroProcesso() {
+        return numeroProcesso;
+    }
+
+    public void setNumeroProcesso(String numeroProcesso) {
+        this.numeroProcesso = numeroProcesso;
+    }
+
+    public String getVara() {
+        return vara;
+    }
+
+    public void setVara(String vara) {
+        this.vara = vara;
+    }
+
+    public String getComarca() {
+        return comarca;
+    }
+
+    public void setComarca(String comarca) {
+        this.comarca = comarca;
+    }
+
+    public ProcessoJudicialStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ProcessoJudicialStatus status) {
+        this.status = status;
+    }
+}
