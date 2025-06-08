847cd3b7815bc254971c0249558b86a11b75f7e8
lucas-PI <lucas22inacio@hotmail.com>
2025-06-08 15:12:51 -0300

    ProcessoJudicialRepository finalizado com os metodos exigidos criados

--- src/main/java/com/incaas/gestaoprocessojuri/repositories/ProcessoJudicialRepository.java
+++ src/main/java/com/incaas/gestaoprocessojuri/repositories/ProcessoJudicialRepository.java
@@ -1,9 +1,17 @@
 package com.incaas.gestaoprocessojuri.repositories;
 
 import com.incaas.gestaoprocessojuri.model.ProcessoJudicial;
+import com.incaas.gestaoprocessojuri.model.enums.ProcessoJudicialStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProcessoJudicialRepository extends JpaRepository<ProcessoJudicial,Long> {
+
+    List<ProcessoJudicial> findAllByComarcaIgnoreCase(String comarca);
+    List<ProcessoJudicial> findAllByStatus(ProcessoJudicialStatus Status);
+    List<ProcessoJudicial> findAllByComarcaIgnoreCaseAndStatus(String comarca,ProcessoJudicialStatus Status);
+
 }
