273f4c340aef76b2047a4e5e7e492897e70b3996
lucas-PI <lucas22inacio@hotmail.com>
2025-06-08 08:54:41 -0300

    Audienciarepository criado

--- src/main/java/com/incaas/gestaoprocessojuri/repositories/AudienciaRepository.java
+++ src/main/java/com/incaas/gestaoprocessojuri/repositories/AudienciaRepository.java
@@ -1,2 +1,9 @@
-package com.incaas.gestaoprocessojuri.repositories;public interface AudienciaRepository {
+package com.incaas.gestaoprocessojuri.repositories;
+
+import com.incaas.gestaoprocessojuri.model.Audiencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AudienciaRepository extends JpaRepository<Audiencia,Long> {
 }
