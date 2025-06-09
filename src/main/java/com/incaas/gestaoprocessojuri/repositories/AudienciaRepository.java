686fd3b996a99a3a415815f98d17d0e72816d0be
lucas-PI <lucas22inacio@hotmail.com>
2025-06-09 14:17:35 -0300

    Anovos métodos de consulta criados no AudienciaRepository

--- src/main/java/com/incaas/gestaoprocessojuri/repositories/AudienciaRepository.java
+++ src/main/java/com/incaas/gestaoprocessojuri/repositories/AudienciaRepository.java
@@ -4,6 +4,12 @@ import com.incaas.gestaoprocessojuri.model.Audiencia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface AudienciaRepository extends JpaRepository<Audiencia,Long> {
+
+    List<Audiencia> findAllByDataAudienciaAndProcessoJudicial_Comarca(LocalDate data, String comarca);
+
 }
